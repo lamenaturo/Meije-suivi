26271bb11c09965ac87cbb2f59da9117fc8ab943
lamenaturo <lamenaturo@gmail.com>
2026-05-01 12:10:46 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBWKNwcMrk5Phc1sEwYP7YcTuCI5DaPnOc",
+ 
+export const firebaseConfig = {
+  apiKey: "AIzaSyBWKNwcMrk5Phc1sEwYP7Yc8Q2mZ9vXkLs",
   authDomain: "meije-naturo.firebaseapp.com",
   projectId: "meije-naturo",
   storageBucket: "meije-naturo.firebasestorage.app",
   messagingSenderId: "354199771986",
-  appId: "1:354199771986:web:b44f3f10d671dd4b9f388b"
+  appId: "1:354199771986:web:b44f3f10de"
 };
-
+ 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+ 
